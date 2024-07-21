a1c3f42a644ed3ad694b269357f954cc2a3b27ac
ivanuria <ivan.uria@gmail.com>
2024-07-21 12:55:26 +0200

    Fix Static

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const morgan = require("morgan")
 const app = express()
+app.use(express.static("dist"))
 
 morgan.token("payload", (request, response) => {
     if (request.method === "POST") {
